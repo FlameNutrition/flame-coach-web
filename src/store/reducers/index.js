5fc570034b933aca9276ef018c6e83bd6bb7d7cd
nbentoneves <nbento.neves@gmail.com>
2021-04-15 13:07:01 +0100

    #24 - Fix performance issue when refresh app

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
-import AsyncStorage from '@react-native-community/async-storage';
 import { persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
 import authReducer from './authReducer';
 import notificationReducer from './notificationReducer';
 
 const rootPersistConfig = {
   key: 'persistStore',
-  storage: AsyncStorage,
-  blacklist: ['auth']
+  storage: storageSession,
+  blacklist: ['auth'],
+  // FIXME: This is a bug: https://github.com/rt2zz/redux-persist/issues/786
+  timeout: 1
 };
 
 // If you want whitelist a specific value please follow the following example
@@ -15,7 +17,7 @@ const rootPersistConfig = {
 // eslint-disable-next-line no-unused-vars
 const authPersistConfig = {
   key: 'auth',
-  storage: AsyncStorage,
+  storage: storageSession,
   blacklist: ['error']
 };
 
