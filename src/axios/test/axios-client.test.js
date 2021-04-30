a6995afa955e58d7ef7b6314dd4c6c36405d3b39
nbentoneves <nbento.neves@gmail.com>
2021-04-30 12:48:58 +0100

    #25 - Create unit tests for axios methods

---
+++ src/axios/test/axios-client.test.js
@@ -0,0 +1,100 @@
+import {
+  getClientContactInformation,
+  getClientPersonalData,
+  updateClientContactInformation,
+  updateClientPersonalData
+} from '../axios-client';
+
+import mockAxios from 'axios';
+
+describe('axios-client tests', () => {
+  it('get client contact information successfully from API', async () => {
+    mockAxios.mockResolvedValueOnce({ data: { response: 'data' } });
+
+    const result = await getClientContactInformation('a65ad74f-4f9f-4b00-98ee-756e454da73f');
+
+    expect(mockAxios).toBeCalledWith({
+      method: 'get',
+      url: '/client/getContactInformation',
+      headers: {
+        clientIdentifier: 'a65ad74f-4f9f-4b00-98ee-756e454da73f'
+      }
+    });
+    expect(result).toEqual({ response: 'data' });
+  });
+
+  it('update client contact information successfully from API', async () => {
+    mockAxios.mockResolvedValueOnce({ data: { response: 'data' } });
+
+    const newContactInformation = {
+      firstName: 'First Name',
+      lastName: 'Last Name',
+      phoneCode: '+44',
+      phoneNumber: '2233445566',
+      countryCode: 'BR'
+    };
+
+    const result = await updateClientContactInformation('a65ad74f-4f9f-4b00-98ee-756e454da73f', newContactInformation);
+
+    expect(mockAxios).toBeCalledWith({
+      method: 'post',
+      url: '/client/updateContactInformation',
+      headers: {
+        clientIdentifier: 'a65ad74f-4f9f-4b00-98ee-756e454da73f',
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({
+        firstName: 'First Name',
+        lastName: 'Last Name',
+        phoneCode: '+44',
+        phoneNumber: '2233445566',
+        countryCode: 'BR'
+      })
+    });
+    expect(result).toEqual({ response: 'data' });
+  });
+
+  it('get client personal data successfully from API', async () => {
+    mockAxios.mockResolvedValueOnce({ data: { response: 'data' } });
+
+    const result = await getClientPersonalData('a65ad74f-4f9f-4b00-98ee-756e454da73f');
+
+    expect(mockAxios).toBeCalledWith({
+      method: 'get',
+      url: '/client/getPersonalData',
+      headers: {
+        clientIdentifier: 'a65ad74f-4f9f-4b00-98ee-756e454da73f'
+      }
+    });
+    expect(result).toEqual({ response: 'data' });
+  });
+
+  it('update client personal information successfully from API', async () => {
+    mockAxios.mockResolvedValueOnce({ data: { response: 'data' } });
+
+    const newPersonalInformation = {
+      weight: 60.5,
+      height: 175,
+      genderCode: 'M',
+      measureTypeCode: 'KM_CM'
+    };
+
+    const result = await updateClientPersonalData('a65ad74f-4f9f-4b00-98ee-756e454da73f', newPersonalInformation);
+
+    expect(mockAxios).toBeCalledWith({
+      method: 'post',
+      url: '/client/updatePersonalData',
+      headers: {
+        clientIdentifier: 'a65ad74f-4f9f-4b00-98ee-756e454da73f',
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({
+        weight: 60.5,
+        height: 175,
+        genderCode: 'M',
+        measureTypeCode: 'KM_CM'
+      })
+    });
+    expect(result).toEqual({ response: 'data' });
+  });
+});
---
+++ src/axios/test/axios-enrollment-process.test.js
@@ -0,0 +1,81 @@
+import {
+  enrollmentProcessBreak,
+  enrollmentProcessFinish,
+  enrollmentProcessInit,
+  enrollmentProcessStatus
+} from '../axios-enrollment-process';
+
+import mockAxios from 'axios';
+
+describe('axios-enrollment-process tests', () => {
+  it('init enrollment process successfully from API', async () => {
+    mockAxios.mockResolvedValueOnce({ data: { response: 'data' } });
+
+    const result = await enrollmentProcessInit('a65ad74f-4f9f-4b00-98ee-756e454da73f', 'f2c9ada9-05f1-4225-a724-6858d2a80c68');
+
+    expect(mockAxios).toBeCalledWith({
+      method: 'post',
+      url: '/client/enrollment/init',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({
+        client: 'a65ad74f-4f9f-4b00-98ee-756e454da73f',
+        coach: 'f2c9ada9-05f1-4225-a724-6858d2a80c68',
+      })
+    });
+    expect(result).toEqual({ response: 'data' });
+  });
+
+  it('finish enrollment process successfully from API', async () => {
+    mockAxios.mockResolvedValueOnce({ data: { response: 'data' } });
+
+    const result = await enrollmentProcessFinish('a65ad74f-4f9f-4b00-98ee-756e454da73f', true);
+
+    expect(mockAxios).toBeCalledWith({
+      method: 'post',
+      url: '/client/enrollment/finish',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({
+        client: 'a65ad74f-4f9f-4b00-98ee-756e454da73f',
+        accept: true,
+      })
+    });
+    expect(result).toEqual({ response: 'data' });
+  });
+
+  it('break enrollment process successfully from API', async () => {
+    mockAxios.mockResolvedValueOnce({ data: { response: 'data' } });
+
+    const result = await enrollmentProcessBreak('a65ad74f-4f9f-4b00-98ee-756e454da73f');
+
+    expect(mockAxios).toBeCalledWith({
+      method: 'post',
+      url: '/client/enrollment/break',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({
+        client: 'a65ad74f-4f9f-4b00-98ee-756e454da73f'
+      })
+    });
+    expect(result).toEqual({ response: 'data' });
+  });
+
+  it('status enrollment process successfully from API', async () => {
+    mockAxios.mockResolvedValueOnce({ data: { response: 'data' } });
+
+    const result = await enrollmentProcessStatus('a65ad74f-4f9f-4b00-98ee-756e454da73f');
+
+    expect(mockAxios).toBeCalledWith({
+      method: 'get',
+      url: '/client/enrollment/status',
+      headers: {
+        clientUUID: 'a65ad74f-4f9f-4b00-98ee-756e454da73f'
+      }
+    });
+    expect(result).toEqual({ response: 'data' });
+  });
+});
