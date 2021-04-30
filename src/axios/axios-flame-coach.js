f9cc56afccb8b1cfb610f67a5ea436015e5ca04e
nbentoneves <nbento.neves@gmail.com>
2021-04-30 08:54:28 +0100

    Configure new .env properties

--- src/axios/axios-flame-coach.js
+++ src/axios/axios-flame-coach.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_SERVER}:${process.env.REACT_APP_API_PORT}/api`
 });
 
 export default instance;
