46f6a21886da8e67366bee2a52f7fbae0d0d4b54
nbentoneves <nbento.neves@gmail.com>
2021-06-21 09:43:41 +0100

    #47 - Update all API services calls with authentication

--- src/api/axios/axios-flame-coach.js
+++ src/api/axios/axios-flame-coach.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_SERVER}:${process.env.REACT_APP_API_PORT}/api`
+  baseURL: `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_SERVER}:${process.env.REACT_APP_API_PORT}/api`,
+  headers: {
+    Authorization: `Basic ${process.env.REACT_APP_API_AUTH}`
+  }
 });
 
 export default instance;
