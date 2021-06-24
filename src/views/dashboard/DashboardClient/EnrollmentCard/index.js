ee885bec4ba20606eeb895553c888eea90ce36c9
nbentoneves <nbento.neves@gmail.com>
2021-06-24 16:07:51 +0100

    Add spinner for all views

---
+++ src/views/dashboard/DashboardClient/EnrollmentCard/index.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  IconButton,
+  makeStyles,
+  Step,
+  StepLabel,
+  Stepper
+} from '@material-ui/core';
+import {
+  NavigateBefore as NavigateBackIcon,
+  NavigateNext as NavigateNextIcon
+} from '@material-ui/icons';
+import { Alert } from '@material-ui/lab';
+import ErrorMessage from '../../../../components/Notification/ErrorMessage/ErrorMessage';
+import InfoMessage from '../../../../components/Notification/InfoMessage/InfoMessage';
+import PropTypes from 'prop-types';
+import Loading from '../../../../components/Loading';
+
+const useStyles = makeStyles(() => ({
+  root: {
+  },
+  coachConfirmation: {
+    display: 'flex',
+    flexFlow: 'row-reverse',
+    paddingRight: '20px'
+  },
+  coachConfirmationNextBtn: {
+    margin: '3px'
+  },
+  coachConfirmationBackBtn: {
+    margin: '3px'
+  }
+}));
+
+const EnrollmentCard = ({
+  isLoading,
+  activeCoachStep, setActiveCoachStep, enrollmentFinish,
+  customerIdentifier, enrollmentStatus
+}) => {
+  const classes = useStyles();
+
+  const steps = ['Waiting for a coach invitation', 'Do you want be part of this experience?', 'Confirmation'];
+
+  return (
+    <Card>
+      <CardContent>
+        {isLoading ? <Loading />
+          : (
+            <>
+              <Stepper activeStep={activeCoachStep} alternativeLabel>
+                {steps.map((label) => (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+
+              <div className={classes.coachConfirmation}>
+                {activeCoachStep === steps.length ? (
+                  <IconButton
+                    aria-label="Reset"
+                    disabled
+                    onClick={() => setActiveCoachStep(0)}
+                  />
+                ) : (
+                  <div>
+                    <IconButton
+                      color="primary"
+                      aria-label="Back"
+                      className={classes.coachConfirmationBackBtn}
+                      disabled={activeCoachStep === 0}
+                      onClick={() => {
+                        if (activeCoachStep === 1) {
+                          enrollmentFinish.mutate({
+                            customerIdentifier,
+                            flag: false
+                          });
+                        } else {
+                          setActiveCoachStep((prevState) => prevState - 1);
+                        }
+                      }}
+                    >
+                      <NavigateBackIcon />
+                    </IconButton>
+
+                    <IconButton
+                      color="primary"
+                      aria-label={activeCoachStep === steps.length - 1 ? 'Finish' : 'Next'}
+                      className={classes.coachConfirmationNextBtn}
+                      disabled={enrollmentStatus === 'AVAILABLE'}
+                      onClick={() => {
+                        if (activeCoachStep === 2) {
+                          enrollmentFinish.mutate({
+                            customerIdentifier,
+                            flag: true
+                          });
+                        } else {
+                          setActiveCoachStep((prevState) => prevState + 1);
+                        }
+                      }}
+                    >
+                      <NavigateNextIcon />
+                    </IconButton>
+                  </div>
+                )}
+              </div>
+              {activeCoachStep === steps.length - 1 ? (
+                <div className={classes.coachConfirmation}>
+                  <Alert severity={ErrorMessage.CODE_0004.level.toLowerCase()}>
+                    {' '}
+                    {ErrorMessage.CODE_0004.msg}
+                  </Alert>
+                </div>
+              ) : null}
+
+              {activeCoachStep === steps.length ? (
+                <div className={classes.coachConfirmation}>
+                  <Alert severity={InfoMessage.CODE_0001.level.toLowerCase()}>
+                    {' '}
+                    {InfoMessage.CODE_0001.msg}
+                  </Alert>
+                </div>
+              ) : null}
+            </>
+          )}
+      </CardContent>
+    </Card>
+  );
+};
+
+EnrollmentCard.propTypes = {
+  activeCoachStep: PropTypes.number.isRequired,
+  setActiveCoachStep: PropTypes.func.isRequired,
+  enrollmentFinish: PropTypes.func.isRequired,
+  customerIdentifier: PropTypes.string.isRequired,
+  enrollmentStatus: PropTypes.string.isRequired,
+  isLoading: PropTypes.bool.isRequired
+};
+
+export default EnrollmentCard;
