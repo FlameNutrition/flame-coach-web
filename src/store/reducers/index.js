5c1b32eca5fed404fcbb3753eb7bfb990dc3bb8d
nbentoneves <nbento.neves@gmail.com>
2021-07-27 08:45:02 +0100

    #28 - Remove auth reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import authReducer from './authReducer';
 import notificationReducer from './notificationReducer';
 
 export default combineReducers({
-  auth: authReducer,
   notification: notificationReducer
 });
